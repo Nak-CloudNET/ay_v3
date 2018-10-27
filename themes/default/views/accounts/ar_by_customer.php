cd11f8f113640995a010cd374b022e9c71f244da
Veang Vicheth <veangvicheth20@gmail.com>
2018-10-27 11:29:21 +0700

    Updated remove all report header.

--- themes/default/views/accounts/ar_by_customer.php
+++ themes/default/views/accounts/ar_by_customer.php
@@ -201,7 +201,30 @@ echo form_open('account/arByCustomer_actions', 'id="action-form"');
                         </div>
 
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"></div>
-
+                        <!--<div class="col-lg-4 col-md-4 col-sm-4">
+                            <br>
+                            <br>
+                            <span><?php echo date("F d, Y"); ?></span><br>
+                            <span><?php echo date("h:i a") ?></span>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4">
+                            <h3 class="text-center" style="font-size: 22px">
+                                <?php
+                        echo $billers->company;
+                        ?>
+                            </h3>
+                            <h3 class="text-center" style="font-size: 22px">Collections report</h3>
+                            <p class="text-center">As of
+                                <?php
+                        if($start_date != NULL){
+                            echo $start_date;
+                        }else{
+                            echo date("F d, Y");
+                        }
+                        ?>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4"></div>-->
                     </div>
                 </div>
                 <div class="table-responsive">
